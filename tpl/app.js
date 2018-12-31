c7e5fec8ebcc9f3d4b0c471522ba4e702728e7a0
soulsands <a407221377@gmail.com>
2018-12-31 21:54:24 +0800

    保存

--- tpl/app.js
+++ tpl/app.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 const marked = require('marked');
+var md = require('markdown-it')();
+
 const os = require('os');
 // debugger
 // let reg =  /^( *)(bull) [^\n]*(?:\n(?!\1bull )[^\n]*)*/;
@@ -40,9 +42,9 @@ marked.setOptions({
 });
 
 let tmp = fs.readFileSync('./tpl.html', 'utf8');
-let str = fs.readFileSync('./md.md', 'utf8');
+let str = fs.readFileSync('./md1.md', 'utf8');
 let html = marked(str)
 //去掉头部
 html = html.replace(/<h.+\n.+typora-copy-i.+\n.+\d>/, '').replace(/^<hr>/, '')
 tmp = tmp.replace('<article id="md-content">', '<article id="md-content">' + os.EOL + html)
-fs.writeFileSync('./md.html', tmp)
+fs.writeFileSync('./md1.html', tmp)
